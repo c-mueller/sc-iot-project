faa99a5157ce293176e890833e393cbec52508fb
Christian Müller <dev@c-mueller.xyz>
2021-07-10 23:06:48 +0200

    Update Makefile

--- simulator/core/http_server.go
+++ simulator/core/http_server.go
@@ -5,6 +5,7 @@ import (
 	util "github.com/c-mueller/sc-iot-project/simulator/utils"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gobuffalo/packr"
 	"github.com/sirupsen/logrus"
 )
 
@@ -25,6 +26,11 @@ func (s *Simulator) InitializeApi() {
 	s.engine.Use(cors.Default())
 	s.engine.Use(util.LogEnricherMiddleware(s.logger))
 	s.engine.Use(util.LogMiddleware(s.logger, true))
+
+	s.engine.StaticFS("/ui", packr.NewBox("ui-bin"))
+	s.engine.GET("/", func(context *gin.Context) {
+		context.Redirect(301, "/ui")
+	})
 
 	apiRouteGroup := s.engine.Group("/api")
 	apiRouteGroup.GET("/sensors", s.ListSensors)
--- simulator/go.mod
+++ simulator/go.mod
@@ -8,7 +8,13 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.3.5 // indirect
 	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.2
+	github.com/gobuffalo/envy v1.9.0 // indirect
+	github.com/gobuffalo/packd v1.0.0 // indirect
+	github.com/gobuffalo/packr v1.30.1 // indirect
 	github.com/google/uuid v1.2.0
+	github.com/markbates/oncer v0.0.0-20181014194634-05fccaae8fc4 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/yaml.v2 v2.4.0 // indirect
